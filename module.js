af216937369ff968bbb8fae208e0980f6fdb1938
basi3467 <58439387+basi3467@users.noreply.github.com>
2020-07-07 17:11:07 +0530

    Create module.js

---
+++ module.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../connection.js/dbconnect')
+
+var Heroes = sequelize.define('heroes', {
+    name: {
+      type: Sequelize.STRING
+    },
+    type: {
+      type: Sequelize.STRING
+    }
+  }, {
+    freezeTableName: true
+  });
+
+  //Posts.sync({force: true}).then(function () {// if we use force:true then it will drop the existing table and creates new ones
+ 
+  module.exports = Heroes
