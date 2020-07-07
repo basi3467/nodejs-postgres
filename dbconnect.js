b41abd21b073175224e258c2a040401a6d7e77be
basi3467 <58439387+basi3467@users.noreply.github.com>
2020-07-07 17:12:42 +0530

    Create dbconnect.js

---
+++ dbconnect.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('dvdrental', 'postgres', 'postgres', {
+    host: 'localhost',
+    dialect: 'postgres',
+    pool: {
+      max: 9,
+      min: 0,
+      idle: 10000
+    }
+  });
+//const sequelize = new Sequelize('postgres://postgres:postgres@server.com:portnumber/dvdrental');
+sequelize.authenticate().then(() => { //.authenticate() is used to check if connection is established or not which is 
+  console.log("Success!");   
+           // a promise so we are using .then to do what if connection is established  
+}).catch((err) => {
+  console.log(err);
+});
+
+module.exports = sequelize;
